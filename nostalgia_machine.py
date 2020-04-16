d3e97011675f49451aa0049aa7937320ed1ff51f
BryanColeman <60757049+BryanColeman@users.noreply.github.com>
2020-04-16 14:53:10 -0400

    Update nostalgia_machine.py

--- nostalgia_machine.py
+++ nostalgia_machine.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import numpy as np
-
-
+import sqlite3
+from sqlite3 import Error
+import math
 
 '''
 we are going to cut up the image into 50x50 pixel pieces so
@@ -48,12 +49,63 @@ def break_into_tiles(image,rgb_ave):
             temp_rgb = (r_average//counter,g_average//counter,b_average//counter)
             rgb_ave.append(temp_rgb)
 
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def query_rgb_values(conn,current_tile):
+    c = conn.cursor()
+    c.execute('SELECT * FROM spongebob_rgb')
+
+    rows = c.fetchall()
+
+    current_min = ['placeholder',100000]
+
+    for row in rows:
+        track_min(row,current_tile,current_min)
+
+    return current_min[0]
+
+
+def track_min(row,current_tile,current_min)
+    '''
+    find the value that is the least meaning
+    it is the closest in distance to the portion of the
+    image we want to replace
+    '''
+    '''
+    sqrt((r2-r1)^2 + (g2-g1)^2 + (b2-b1^2))
+    is this less then it currently is?
+    this is the new image we use
+    '''
+
+    s_red = row[1]
+    s_green = row[2]
+    s_blue = row[3]
+    t_red = current_tile[1]
+    t_green = current_tile[2]
+    t_blue = current_tile[3]
+
+    result = math.sqrt(math.pow(s_red - t_red, 2) + math.pow(s_green - t_green, 2) + math.pow(s_blue - t_blue, 2))
+
+    if result < current_min[1]:
+        current_min[0] = row[0]
+        current_min[1] = result
+
+
 def main(pic):
     rgb_ave = []
     image = Image.open(pic)      #simple test will need to ask user for picture
     image = resize(image)
     width, height = image.size
     break_into_tiles(image,rgb_ave)
+
+    conn = create_connection(r'images_rgb.db')
 
     new_im = Image.new('RGB',(width,height))
     i = 0
@@ -64,7 +116,8 @@ def main(pic):
             will need to find the source image here
             by using distance equation again all source image rgb values
             '''
-            temp_im = Image.new('RGB',(25,25),rgb_ave[i])
+            #script -> finds closest image
+            temp_im = Image.open(query_rgb_values(conn,rgb_ave[i]))
             i += 1
             (left,upper,right,lower) = (lef*25,upp*25,lef*25+25,upp*25+25)
             new_im.paste(temp_im,(left,upper,right,lower))
